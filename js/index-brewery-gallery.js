c488f39adf8d4aaa28573bc7dd3ebe2a52944821
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-06-29 23:09:30 +0300

    Divs deleted in js

--- js/index-brewery-gallery.js
+++ js/index-brewery-gallery.js
@@ -48,32 +48,32 @@
 
 (function () {
   const slides = [
-    `<div>
+    `
     <img src="img/brewery-gallery/gallery2-photo1.png"
         alt="Two bearded men in a brewery with bottles on the foreground"
-        class="brewery-gallery__img--main slide1">
-    </div>`,
-    `<div>
+        class="brewery-gallery__img--main">
+    `,
+    `
     <img src="img/brewery-gallery/gallery2-photo2.png" alt="Beer barrels in a brewery"
-        class="brewery-gallery__img--main slide2">
-    </div>`,
-    `<div>
+        class="brewery-gallery__img--main">
+    `,
+    `
     <img src="img/brewery-gallery/gallery2-photo3.png" alt="beer bottles production line"
-        class="brewery-gallery__img--main slide3">
-    </div>`,
-    `<div>
+        class="brewery-gallery__img--main">
+    `,
+    `
     <img src="img/brewery-gallery/gallery2-photo4.png" alt="A bearded man holding a beer barrel"
-        class="brewery-gallery__img--main slide4">
-    </div>`,
-    `<div>
+        class="brewery-gallery__img--main">
+    `,
+    `
     <img src="img/brewery-gallery/gallery2-photo5.png" alt="Two men working in a brewery"
-        class="brewery-gallery__img--main slide5">
-    </div>`,
-    `<div>
+        class="brewery-gallery__img--main">
+    `,
+    `
     <img src="img/brewery-gallery/gallery2-photo6.png"
         alt="Close-up of brown beer bottles on a production line"
-        class="brewery-gallery__img--main slide6">
-    </div>`,
+        class="brewery-gallery__img--main">
+    `,
   ];
 
   let slideIdx = 0;
@@ -88,11 +88,13 @@
   function nextSlide() {
     slideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
     showCurrentSlide();
+    // slideContainer.style.transition = "left 2s";
   }
 
   function prevSlide() {
     slideIdx = slideIdx - 1 < 0 ? slides.length - 1 : slideIdx - 1;
     showCurrentSlide();
+    // slideContainer.style.transition = "right 2s";
   }
 
   showCurrentSlide();
