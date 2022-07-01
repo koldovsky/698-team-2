fc8d4f87675eccb569539a861fac56e61baba09e
Mariia Kanafotska <mariia.kanafotska@gmail.com>
2022-07-01 11:03:07 +0000

    accordion js added

---
+++ js/index-accordion.js
@@ -0,0 +1,13 @@
+var acc = document.getElementsByClassName("question__acc-button");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    //this.classList.toggle("active");
+    var panel = this.parentElement.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
