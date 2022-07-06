42b028351a94fd97f8b001f2942596fecfa41a0e
oleksandr-ukr <zlenko22@gmail.com>
2022-07-06 19:22:39 +0000

    swiperzlenko

--- js/index-clock.js
+++ js/index-clock.js
@@ -4,3 +4,33 @@ function undateClock() {
     clockConteiner.innerText = new Date().toLocaleTimeString();
 }
 setInterval(undateClock, 1000);
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    gragCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
